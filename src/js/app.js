e1a5f7c6d4c65e54131d3596e4407c3ade75bbb8
Polyakova Maria <polyakovamy@yandex.ru>
2022-12-08 11:41:14 +0300

    Получение данных с сервера и отображение в модальном окне

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,80 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-console.log("hello");
+let elems = document.querySelector(".location-elems");
+let locationBody = document.querySelector(".location-body");
+let preloader = document.querySelector(".preloader");
+
+let locationModal = document.querySelector(".location__modal");
+let location = document.querySelector(".location");
+let locationClose = document.querySelector(".location-search__close");
+let locationInput = document.querySelector(".location-search__input");
+
+async function getResponse() {
+  // Скрытие данных и отображение прелодера
+  locationBody.classList.add("hide");
+  preloader.classList.remove("hide");
+
+  // Запрос к серверу
+  const url = "https://studika.ru/api/areas";
+  let response = await fetch(url, { method: "POST" });
+  let content = await response.json();
+
+  // Все города с округами
+  let cities = [];
+  for (let state in content) {
+    if ("cities" in content[state]) {
+      for (let city in content[state].cities) {
+        cities.push({
+          name: content[state].cities[city].name,
+          state: content[state].name,
+        });
+      }
+    } else cities.push({ state: content[state].name });
+  }
+
+  // Отображение данных, скрытие прелодера, фокусировка на инпуте
+  locationBody.classList.remove("hide");
+  preloader.classList.add("hide");
+  locationInput.focus();
+
+  for (let key in cities.splice(0, 80)) {
+    if ("name" in cities[key]) {
+      elems.innerHTML += `
+          <div class="location-elem">
+            <span class="location-elem__city">${cities[key].name}</span>
+            <span class="location-elem__city-state">${cities[key].state}</span>
+          </div>`;
+    } else {
+      elems.innerHTML += `
+          <div class="location-elem">
+            <span class="location-elem__city">${cities[key].state}</span>
+          </div>`;
+    }
+  }
+}
+
+// Отображение локального окна для поиска города
+
+location.addEventListener("click", () => {
+  locationModal.classList.toggle("hide");
+  if (!locationModal.classList.contains("hide")) {
+    locationInput.focus();
+    getResponse();
+  }
+});
+
+// Добавление крестика при фокусировке на инпуте
+locationInput.addEventListener("focus", () =>
+  locationClose.classList.remove("hide")
+);
+
+locationInput.addEventListener("blur", () =>
+  locationClose.classList.add("hide")
+);
+
+// Очистка инпута городов
+locationClose.addEventListener("click", () => {
+  locationInput.value = "";
+  locationInput.focus();
+});
