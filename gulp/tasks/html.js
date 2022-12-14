40cbd8b5d20f9140680a07ca19ec0292fb7a7d71
Polyakova Maria <polyakovamy@yandex.ru>
2022-12-14 17:08:55 +0300

    Закомментирование добавления новой версии кода

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -5,33 +5,35 @@ import htmlmin from "gulp-html-minifier";
 import rename from "gulp-rename";
 
 export const html = () => {
-  return app.gulp
-    .src(app.path.src.html)
-    .pipe(
-      app.plugins.plumber(
-        app.plugins.notify.onError({
-          title: "HTML",
-          message: "Error <%= error.message %>",
-        })
+  return (
+    app.gulp
+      .src(app.path.src.html)
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
+            title: "HTML",
+            message: "Error <%= error.message %>",
+          })
+        )
       )
-    )
-    .pipe(GulpPug({ pretty: true }))
-    .pipe(app.plugins.replace(/@img\//g, "images/"))
-    .pipe(webpHtmlNosvg())
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(app.gulp.dest(app.path.build.html))
-    .pipe(
-      versionNumber({
-        value: "%DT%",
-        append: {
-          key: "_v",
-          cover: 0,
-          to: ["css", "js"],
-        },
-        output: {
-          file: "gulp/version.json",
-        },
-      })
-    )
-    .pipe(app.plugins.browsersync.stream());
+      .pipe(GulpPug({ pretty: true }))
+      .pipe(app.plugins.replace(/@img\//g, "images/"))
+      .pipe(webpHtmlNosvg())
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(app.gulp.dest(app.path.build.html))
+      // .pipe(
+      //   versionNumber({
+      //     value: "%DT%",
+      //     append: {
+      //       key: "_v",
+      //       cover: 0,
+      //       to: ["css", "js"],
+      //     },
+      //     output: {
+      //       file: "gulp/version.json",
+      //     },
+      //   })
+      // )
+      .pipe(app.plugins.browsersync.stream())
+  );
 };
